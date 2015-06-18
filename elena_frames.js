248841a51f777541cba65c2986ec880b6b849d1c
MKCrimson <mad.king.crimson@gmail.com>
2015-06-18 01:56:44 -0400

    elena frame data

---
+++ elena_frames.js
@@ -0,0 +1,12 @@
+ <!DOCTYPE javascript>
+var elena = {
+	crouch_jab_startup: 3,
+	crouch_jab_active: 2,
+	crouch_jab_recovery: 8,
+	crouch_strong_startup: 5,
+	crouch_strong_active: 5,
+	crouch_strong_recovery: 11
+	crouch_fierce_startup: 9,
+	crouch_fierce_active: 3,
+	crouch_fierce_recovery: 22
+}
